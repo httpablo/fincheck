89c6eb76326480f4f575bb71316468201086e536
Pablo Brito <pablobrith@gmail.com>
2025-11-05 22:44:15 -0300

    feat: create categories

---
+++ api/src/modules/bank-accounts/bank-accounts.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { BankAccountsService } from './bank-accounts.service';
+import { CreateBankAccountDto } from './dto/create-bank-account.dto';
+import { ActiveUserId } from 'src/shared/decorators/ActiveUserId';
+
+@Controller('bank-accounts')
+export class BankAccountsController {
+  constructor(private readonly bankAccountsService: BankAccountsService) {}
+
+  @Post()
+  create(
+    @ActiveUserId() userId: string,
+    @Body() createBankAccountDto: CreateBankAccountDto,
+  ) {
+    return this.bankAccountsService.create(userId, createBankAccountDto);
+  }
+
+  @Get()
+  findAll(@ActiveUserId() userId: string) {
+    return this.bankAccountsService.findAllByUserId(userId);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string) {
+    return this.bankAccountsService.update(id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.bankAccountsService.remove(id);
+  }
+}
--- api/src/shared/database/database.module.ts
+++ api/src/shared/database/database.module.ts
@@ -1,10 +1,17 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UserRepository } from './repositories/users.repositories';
+import { CategoriesRepository } from './repositories/categories.repositories';
+import { BankAccountRepository } from './repositories/bank-account.repositories';
 
 @Global()
 @Module({
-  providers: [PrismaService, UserRepository],
-  exports: [UserRepository],
+  providers: [
+    PrismaService,
+    UserRepository,
+    CategoriesRepository,
+    BankAccountRepository,
+  ],
+  exports: [UserRepository, CategoriesRepository, BankAccountRepository],
 })
 export class DatabaseModule {}
