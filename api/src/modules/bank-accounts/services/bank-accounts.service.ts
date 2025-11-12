d0e1b48586a7451d7ddf84c2e9b7894705bcdd13
httpablo <pablobrith@gmail.com>
2025-11-12 13:59:56 -0300

    feat: calculando o currentBalance por bankAccount

--- api/src/modules/bank-accounts/services/bank-accounts.service.ts
+++ api/src/modules/bank-accounts/services/bank-accounts.service.ts
@@ -25,9 +25,35 @@ export class BankAccountsService {
     });
   }
 
-  findAllByUserId(userId: string) {
-    return this.bankAccountsRepo.findMany({
+  async findAllByUserId(userId: string) {
+    const bankAccount = await this.bankAccountsRepo.findMany({
       where: { userId },
+      include: {
+        transactions: {
+          select: {
+            type: true,
+            value: true,
+          },
+        },
+      },
+    });
+
+    return bankAccount.map(({ transactions, ...bankAccount }) => {
+      const totalTransactions = transactions.reduce(
+        (acc, transaction) =>
+          acc +
+          (transaction.type === 'INCOME'
+            ? transaction.value
+            : -transaction.value),
+        0,
+      );
+
+      const currentBalance = bankAccount.initialBalance + totalTransactions;
+
+      return {
+        ...bankAccount,
+        currentBalance,
+      };
     });
   }
 
